5530a31e162f029a7c3db3866aa330a5f66e570b
Camila Wolter <camila0wolter@gmail.com>
2023-05-17 09:34:51 -0300

    test: implementação dos testes para a classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,19 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ovo = Ingredient("ovo")
+    camarao = Ingredient("camarão")
+
+    assert camarao.restrictions == {
+            Restriction.ANIMAL_MEAT,
+            Restriction.SEAFOOD,
+            Restriction.ANIMAL_DERIVED,
+    }
+    assert camarao.name == "camarão"
+    assert camarao.__hash__() == hash("camarão")
+    assert hash(camarao != ovo)
+    assert camarao.__repr__() == "Ingredient('camarão')"
+    assert camarao.__eq__(ovo) is False
+    assert camarao.__eq__(camarao) is True
