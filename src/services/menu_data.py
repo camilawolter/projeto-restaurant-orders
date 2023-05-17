98a2c99334a8b59826da00ed6af703cfe9cf0b13
Camila Wolter <camila0wolter@gmail.com>
2023-05-17 11:00:18 -0300

    feat: implementação da classe MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,32 @@
+import pandas
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = self.create_dishes(source_path)
+
+    def create_dishes(self, source_path: str):
+        dishes = set()
+        dishes_df = pandas.read_csv(source_path)
+
+        previous_dish = None
+        dish = None
+
+        for dish_name, price, ingredient, amount in dishes_df.itertuples(
+            index=False
+        ):
+
+            if dish_name != previous_dish:
+                if previous_dish is not None:
+                    dishes.add(dish)
+                previous_dish = dish_name
+                dish = Dish(dish_name, float(price))
+
+            dish.add_ingredient_dependency(Ingredient(ingredient), amount)
+
+        dishes.add(dish)
+
+        return dishes
